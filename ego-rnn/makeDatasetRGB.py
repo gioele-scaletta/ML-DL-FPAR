3e54e7915a9855c6be800b946f4dc451e21dadaf
drewparo <parolinandrea2@gmail.com>
2021-05-23 14:54:32 +0200

    Updated version
    Commenti + Aggiunte al makeDataset

--- ego-rnn/makeDatasetRGB.py
+++ ego-rnn/makeDatasetRGB.py
@@ -7,57 +7,67 @@ import glob
 import random
 
 # prima di tutto crea la classe con makeDataset, si fa dare la directory con i training frames
+#prova
 
 
-def gen_split(root_dir, stackSize):
-    Dataset = [] #Path to RGB frame
+root_dir = os.path.join('/content/drive/MyDrive/ML_project/ego-rnn/content/GTEA61', '')
+
+
+def gen_split(root_dir,train_dataset_folder, stackSize = 5):
+    Dataset_OpticalFlowX = [] #Path to RGB frame
+    Dataset_OpticalFlowY = []
+    Dataset_RGBFrame = []
     Labels = [] #Labels
     NumFrames = [] #
-    root_dir = os.path.join(root_dir, 'processed_frames2')
-    for dir_user in sorted(os.listdir(root_dir)):
-        print('dir_user' + dir_user)
+    root_dir = os.path.join(root_dir, 'flow_x_processed')
+    for dir_user in train_dataset_folder:
+        print('Splittin in ' + dir_user + ' folder')
         class_id = 0
-        dir = os.path.join(root_dir, dir_user)
-        print('dir' + str(dir))
-        for target in sorted(os.listdir(dir)):
-            dir1 = os.path.join(dir, target)
-            
-            insts = sorted(os.listdir(dir1))
-            if insts != []:
-                for inst in insts:
-                    inst_dir = os.path.join(dir1, inst)
-                    inst_dir = os.path.join(inst_dir, 'rgb')
-                    numFrames = len(glob.glob1(inst_dir, '*.png'))
-                    if numFrames >= stackSize:
-                        Dataset.append(inst_dir)
-                        Labels.append(class_id)
-                        NumFrames.append(numFrames)
-            class_id += 1
-    return Dataset, Labels, NumFrames
+        dir = os.path.join(root_dir, dir_user) #Folder effettiva dove mettere i file
+        action_sorted = sorted(os.listdir(dir))
+        for target in action_sorted:
+          dir1 = os.path.join(dir, target) #/ego-rnn/content/GTEA61/flow_x_processed/S4/close_peanut ,/ego-rnn/content/GTEA61/flow_x_processed/S4/close_mustard ..
+          insts = sorted(os.listdir(dir1)) # folder 1, 2 ,3 in each action
+          if insts != []:
+            for inst in insts:
+              inst_dir = os.path.join(dir1, inst)
+              numFrames = len(glob.glob1(inst_dir, '*[0-9].png')) # nome dei file per ogni azione [0-9 indica numero generico]
+              if numFrames >= stackSize: # numero di frame sufficiente
+                NumFrames.append(numFrames) # numero di frame x ogni folder
+                Labels.append(class_id)
+                Dataset_OpticalFlowX.append(inst_dir)
+                Labels.append(class_id) # numero della classe del azione
+                Dataset_RGBFrame.append(os.path.join(inst_dir.replace('flow_x_processed', 'processed_frames'), "rgb"))
+                #print(inst_dir)
+                Dataset_OpticalFlowY.append(inst_dir.replace('flow_x_processed', 'flow_y_processed'))
+        class_id += 1
+    return Dataset_RGBFrame, Dataset_OpticalFlowX, Dataset_OpticalFlowY, Labels, NumFrames
 
 class makeDataset(Dataset):
-    def __init__(self, root_dir, spatial_transform=None, seqLen=20,
-                 train=True, mulSeg=False, numSeg=1, fmt='.png'):
-
-        self.images, self.labels, self.numFrames = gen_split(root_dir, 5)
+    def __init__(self, root_dir,train_dataset_folder, spatial_transform=None, seqLen=7, train=True):
+        self.dataset_RGBFrame, self.dataset_OpticalFlowX, self.dataset_OpticalFlowY, self.labels, self.numFrames = gen_split(root_dir,train_dataset_folder, 5)
         self.spatial_transform = spatial_transform
         self.train = train
-        self.mulSeg = mulSeg
-        self.numSeg = numSeg
         self.seqLen = seqLen
-        self.fmt = fmt
+        self.stackSize = 5
+        self.fmt = '.png'
 
     def __len__(self):
-        return len(self.images)
+        return len(self.Dataset_OpticalFlowX)
 
-    def __getitem__(self, idx):
-        vid_name = self.images[idx]
+    def __getitem__(self, idx): #dataset[idx]
+        vid_nameX = self.dataset_OpticalFlowX[idx]
+        vid_nameY = self.dataset_OpticalFlowY[idx]
+        vid_nameF = self.Dataset_RGBFrame[idx]
+
         label = self.labels[idx]
-        numFrame = self.numFrames[idx]
+        numFrame = self.NumFrames[idx]
+
         inpSeq = []
+
         self.spatial_transform.randomize_parameters()
-        for i in np.linspace(1, numFrame, self.seqLen, endpoint=False):
-            fl_name = vid_name + '/' + 'rgb' + str(int(np.floor(i))).zfill(4) + self.fmt
+        for i in np.linspace(1, numFrame, self.seqLen, endpoint=False): 
+            fl_name = vid_nameX + '/' + 'rgb' + str(int(np.floor(i))).zfill(4) + self.fmt
             img = Image.open(fl_name)
             inpSeq.append(self.spatial_transform(img.convert('RGB')))
         inpSeq = torch.stack(inpSeq, 0)
