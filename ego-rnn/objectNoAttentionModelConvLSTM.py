4de95299f7a5e0eab44e04056928906200d6266b
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-05-25 19:45:30 +0200

    Create objectNoAttentionModelConvLSTM.py

---
+++ ego-rnn/objectNoAttentionModelConvLSTM.py
@@ -0,0 +1,30 @@
+import torch
+import resnetMod
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+from MyConvLSTMCell import *
+
+
+class no_attentionModel(nn.Module):
+    def __init__(self, num_classes=61, mem_size=512):
+        super(attentionModel, self).__init__()
+        self.num_classes = num_classes
+        self.resNet = resnetMod.resnet34(True, True)
+        self.mem_size = mem_size
+        self.weight_softmax = self.resNet.fc.weight
+        self.lstm_cell = MyConvLSTMCell(512, mem_size)
+        self.avgpool = nn.AvgPool2d(7)
+        self.dropout = nn.Dropout(0.7)
+        self.fc = nn.Linear(mem_size, self.num_classes)
+        self.classifier = nn.Sequential(self.dropout, self.fc)
+
+    def forward(self, inputVariable):
+        state = (Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()),
+                 Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()))
+        for t in range(inputVariable.size(0)):
+            logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
+            state = self.lstm_cell(feature_conv, state)
+        feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
+        feats = self.classifier(feats1)
+        return feats, feats1
