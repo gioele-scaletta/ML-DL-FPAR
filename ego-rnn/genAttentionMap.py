ab207821102058edf201c3cab72d96d317b33276
drewparo <parolinandrea2@gmail.com>
2021-05-31 11:04:09 +0200

    updateVersion

--- ego-rnn/genAttentionMap.py
+++ ego-rnn/genAttentionMap.py
@@ -26,16 +26,39 @@ attentionMapModel.train(False)
 
 for params in attentionMapModel.parameters():
     params.requires_grad = False
-    
-normalize = transforms.Normalize(
-   mean=[0.485, 0.456, 0.406],
-   std=[0.229, 0.224, 0.225]
-)
-preprocess1 = transforms.Compose([
-   transforms.Resize(256),
-   transforms.CenterCrop(224),
-])
 
-preprocess2 = transforms.Compose([
-    transforms.ToTensor(),
-    normalize])
+def applyAttentionToRGB(rgb_folder, output_folder):
+  normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+  preprocess1 = transforms.Compose([transforms.Resize(256),transforms.CenterCrop(224)])
+  preprocess2 = transforms.Compose([transforms.ToTensor(),normalize])
+
+  img_array = [rgb_folder+x for x in os.listdir(rgb_folder) if x.endswith("png")]
+  img_array.sort() # tiene ordine se frame numerati
+  n_rgb_frames=0
+
+  img_array_res_np = []
+  for img in img_array:
+    fl_name_in = img
+    fl_name_out = output_folder+ img.split("/")[-1]
+
+    img_pil = Image.open(fl_name_in)
+    img_pil1 = preprocess1(img_pil)
+    img_size = img_pil1.size
+    size_upsample = (img_size[0], img_size[1])
+    img_tensor = preprocess2(img_pil1)
+    img_variable = Variable(img_tensor.unsqueeze(0).cuda())
+    img = np.asarray(img_pil1)
+    attentionMap_image = attentionMapModel(img_variable, img, size_upsample)
+    if cv2.imwrite(fl_name_out, attentionMap_image):
+      n_rgb_frames=n_rgb_frames+1
+      img_array_res_np.append(np.uint8(attentionMap_image))
+  return n_rgb_frames,img_array_res_np
+
+class makeAttentionImage():
+    def __init__(self, rgb_folder, output_folder):
+        self.folder_input=rgb_folder
+        self.output_folder=output_folder
+        self.nOfFrames,self.imageWithAttentionNP = applyAttentionToRGB(rgb_folder,output_folder)
+
+video1 = makeAttentionImage('./testImage/test/','./testImage/res/')
+imageio.mimsave('./testImage/res/file.gif',video1.imageWithAttentionNP,fps=8)
