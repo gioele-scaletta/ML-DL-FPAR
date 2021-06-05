a9c7384cd1876d120dc2400236908ee1b6ba84fc
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-05 12:25:24 +0200

    Update ss_task.py

--- ego-rnn/ss_task.py
+++ ego-rnn/ss_task.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import math
 import torch
-from torch.nn.functional import relu
+from torch.nn.functional import sigmoid, softmax, relu
 
 def conv1x1(in_planes, out_planes, kernel_size=1, stride=1, padding=0):
     return nn.Conv2d(in_channels=in_planes, out_channels=out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
@@ -10,18 +10,18 @@ class ss_task(nn.Module):
     def __init__(self):
         super(ss_task, self).__init__()
         self.conv = conv1x1(in_planes=512, out_planes=100, kernel_size=1, stride=1, padding=0)
-        self.fc = nn.Linear(100*7*7,  2*7*7)
+        self.fc = nn.Linear(100*7*7,  7*7)
         
         #suggested by professor
         #feat (BS,512x7x7) --> relu() = feat (BS,512x7x7)
         #feat (BS,512x7x7) --> CONV[100, k=1, p=0] = feat(BS,100x7x7)
         #feat(BS,100x7x7) --> feat(BS,100*7*7)
-        #feat(BS,100*7*7) --> FC[100*7*7, 2*7*7] = feat(BS, 2*7*7)
+        #feat(BS,100*7*7) --> FC[100*7*7, 2*7*7] = feat(BS, 2*7*7) #the 2 is for classification
         #feat(BS, 2*7*7) --> feat(BS, 2x7x7)
         #cross_entropy(feat, maps)
         
     def forward(self, x):
-        out = torch.relu(x)
+        out = torch.sigmoid(x)
         out = self.conv(out)
         out = torch.flatten(out,1)
         out = self.fc(out)
