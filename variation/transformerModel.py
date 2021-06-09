54eec74b36a73e45194953c5dda5328ad8744783
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-09 11:36:16 +0200

    Update transformerModel.py

--- variation/transformerModel.py
+++ variation/transformerModel.py
@@ -21,16 +21,13 @@ class selfAttentionModel(nn.Module):
         self.classifier = nn.Sequential(self.dropout, self.fc)
 
     def forward(self, inputVariable):
-        predictions = []
+        logits = []
         for t in range(inputVariable.size(0)):
             logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
-            
-		
-	    n_frames, n_channels, h, w = feature_conv.size()	# n_channels = 512 and h x w = 7x7
-	    mbedding = torch.squeeze(torch.squeeze(self.avgpool(feature_conv),3),2)
-	    
-	    logit = self.transf(embedding[k])
-            final_logit = nn.Linear(2048,61)
-	    predictions.append(final_logit)
-	logits.view(inputVariable.size(0), -1)
-        return probs
+        n_frames, n_channels, h, w = feature_conv.size()	# n_channels = 512 and h x w = 7x7
+        embedding = torch.squeeze(torch.squeeze(self.avgpool(feature_conv),3),2)
+        logit = self.transf(embedding)
+        final_logit = nn.Linear(2048,61)
+        logits.append(final_logit)
+    logits.view(inputVariable.size(0), -1)
+    return logits
