f97d98ffb409846d123456c7c87c2df1cd08db67
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-09 16:41:13 +0200

    Update MyTransformer.py

--- variation/MyTransformer.py
+++ variation/MyTransformer.py
@@ -10,8 +10,8 @@ import math
 class MyTransformer(nn.Module):
     def __init__(self):
         super(MyTransformer, self).__init__()
-        self.d_k = 16
-        self.d_v = 16
+        self.d_k = 64
+        self.d_v = 64
         self.d_model = 512
         self.d_ff = 2048
         self.heads = 8
@@ -32,6 +32,7 @@ class MyTransformer(nn.Module):
 
 
     def forward(self,frame):
+        frame = frame + positional_encoding(7,512) 
         multi_head_output = self.temporal_attention(frame)
         transformer_output = self.MLP_head(multi_head_output)
         return transformer_output
@@ -45,6 +46,8 @@ class MyTransformer(nn.Module):
             Value = torch.matmul(frame,self.W_v)
 
             single_head_output = self.self_attention(Query,Key,Value)
+            single_head_output = torch.add(single_head_output, frame)
+            single_head_output = torch.nn.LayerNorm(single_head_output)
             outputs.append(single_head_output)
         heads_concat_output = torch.cat(outputs,axis=1)
 
@@ -64,5 +67,26 @@ class MyTransformer(nn.Module):
     def MLP_head(self,multi_head_output):
         out = self.classifier(multi_head_output)
         out = torch.add(out,multi_head_output)
+        out = torch.nn.LayerNorm(out)
         out = torch.mean(out,1).view(self.d_model) #not really sure if mean is the best thing, BERT and VTN use the CLS token
         return out
+    
+    def get_angles(pos, i, d_model):
+        angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+        return pos * angle_rates
+
+    def positionalencoding(position, d_model):
+        # NOTE the code is from https://www.tensorflow.org/tutorials/text/transformer, not ours
+        angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                          np.arange(d_model)[np.newaxis, :],
+                          d_model)
+  
+        # apply sin to even indices in the array; 2i
+        angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+  
+        # apply cos to odd indices in the array; 2i+1
+        angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    
+        pos_encoding = angle_rads[np.newaxis, ...]
+    
+        return pos_encoding
