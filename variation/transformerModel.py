62b774d915699df4af0503f4b8fd47af601ff197
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-10 22:00:11 +0200

    Update transformerModel.py

--- variation/transformerModel.py
+++ variation/transformerModel.py
@@ -27,8 +27,15 @@ class selfAttentionModel(nn.Module):
         logits = []
         for t in range(inputVariable.size(0)):
             logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
-            n_frames, n_channels, h, w = feature_conv.size()	# n_channels = 512 and h x w = 7x7
-            embedding = torch.squeeze(torch.squeeze(self.avgpool(feature_conv),3),2)
+            bz, nc, h, w = feature_conv.size()    #bz = batch size    #nc = number of channels    #h = height   #w = width
+            feature_conv1 = feature_conv.view(bz, nc, h*w)    #make vector from matrix (hxw -> h*w)
+            probs, idxs = logit.sort(1, True)   #sort in order from highest to lowest the score of each class in each batch
+            class_idx = idxs[:, 0]    
+            cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
+            attentionMAP = F.softmax(cam.squeeze(1), dim=1)
+            attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
+            attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
+            embedding = torch.squeeze(torch.squeeze(self.avgpool(attentionFeat),3),2)
             logit = self.transf(embedding)
             final_logit = self.fc(logit.cuda())
             logits.append(final_logit.view(1,-1))
