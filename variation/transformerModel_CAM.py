2d2a87a3035dffade9376cb7f9949b0d85996b52
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-10 23:51:28 +0200

    Update transformerModel_CAM.py

--- variation/transformerModel_CAM.py
+++ variation/transformerModel_CAM.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from torch.autograd import Variable
-from MyTransformer import *
+from MyTransformer2 import *
 
 
 class selfAttentionModel(nn.Module):
