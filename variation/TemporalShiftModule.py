0a93380a8c16242cefe2c440a17fe1cfbde0783a
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-28 22:59:22 +0200

    Create TemporalShiftModule.py

---
+++ variation/TemporalShiftModule.py
@@ -0,0 +1,17 @@
+from torch import nn
+
+from ops.basic_ops import ConsensusModule
+from ops.transforms import *
+from torch.nn.init import normal_, constant_
+
+def TSM(input, n_frames, fold_div):
+    n, c, h, w = input.size()
+    batch_size = n // n_frames
+    input = input.view(batch_size, n_frames, c, h, w)
+    fold = c // fold_div
+    out = torch.zeros_like(x)
+    out[:, :-1, :fold] = x[:, 1:, :fold]  # shift left
+    out[:, 1:, fold: 2 * fold] = x[:, :-1, fold: 2 * fold]  # shift right
+    out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # not shift
+
+    return out
