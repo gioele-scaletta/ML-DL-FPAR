a44daa3bb9fbdf2f83899b2790ff9ac6daf1f5e7
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-29 15:34:45 +0200

    Update MyTransformer.py

--- variation/MyTransformer.py
+++ variation/MyTransformer.py
@@ -13,47 +13,53 @@ class MyTransformer(nn.Module):
         self.d_model = 512
         self.d_ff = 2048
         self.heads = 8
-        self.W_q = torch.nn.init.xavier_normal_(Variable(torch.randn(self.d_model, self.d_k).type(torch.cuda.DoubleTensor), requires_grad=True))
-        self.W_k = torch.nn.init.xavier_normal_(Variable(torch.randn(self.d_model, self.d_k).type(torch.cuda.DoubleTensor), requires_grad=True))
-        self.W_v = torch.nn.init.xavier_normal_(Variable(torch.randn(self.d_model, self.d_v).type(torch.cuda.DoubleTensor), requires_grad=True))
-        self.W_o = torch.nn.init.xavier_normal_(Variable(torch.randn(self.heads*self.d_v, self.d_model).type(torch.cuda.DoubleTensor), requires_grad=True))
-        self.conv1 = nn.Conv1d(self.d_model, self.d_ff, kernel_size=1, stride=1)
+        self.layer_normalization1 = nn.LayerNorm(512)
+        self.layer_normalization2 = nn.LayerNorm(512)
+        self.W_q = torch.nn.init.xavier_normal_(Variable(torch.randn(self.d_model, self.d_k).type(torch.cuda.FloatTensor), requires_grad=True))
+        self.W_k = torch.nn.init.xavier_normal_(Variable(torch.randn(self.d_model, self.d_k).type(torch.cuda.FloatTensor), requires_grad=True))
+        self.W_v = torch.nn.init.xavier_normal_(Variable(torch.randn(self.d_model, self.d_v).type(torch.cuda.FloatTensor), requires_grad=True))
+        self.W_o = torch.nn.init.xavier_normal_(Variable(torch.randn(self.heads*self.d_v, self.d_model).type(torch.cuda.FloatTensor), requires_grad=True))
+        self.fc1 = nn.Linear(8192, self.d_ff,1)
         self.activation = nn.GELU()
         self.dropout = nn.Dropout(0.1)
-        self.conv2 = nn.Conv1d(self.d_ff, self.d_model, kernel_size=1, stride=1)
-        self.classifier = nn.Sequential(self.conv1,self.activation,self.dropout,self.conv2)
-        nn.init.xavier_normal_(self.conv1.weight)
-        nn.init.constant_(self.conv1.bias, 0)
-        nn.init.xavier_normal_(self.conv2.weight)
-        nn.init.constant_(self.conv2.bias, 0)
+        self.fc2 = nn.Linear(self.d_ff, 8192,1)
+        self.classifier = nn.Sequential(self.fc1,self.activation,self.dropout,self.fc2)
+        nn.init.xavier_normal_(self.fc1.weight)
+        nn.init.constant_(self.fc1.bias, 0)
+        nn.init.xavier_normal_(self.fc2.weight)
+        nn.init.constant_(self.fc2.bias, 0)
 
 
 
     def forward(self,frame):
-        frame_position = self.positionalencoding(frame.size(0))
+        frame_position = self.positionalencoding(frame.size(1))
+        print(frame.size())
+        print(frame_position.size())
         frame2 = frame + frame_position
-        #print(frame.size())
-        multi_head_output = self.temporal_attention(frame.squeeze(0).type(torch.cuda.DoubleTensor))
-        transformer_output = self.MLP_head(multi_head_output.cuda())
+        multi_head_output = self.temporal_attention(frame.squeeze(0))
+        transformer_output = self.MLP_head(multi_head_output)
         return transformer_output
 
 
     def temporal_attention(self,frame):
         outputs = []
-        for i in range(self.heads):
-            Query = torch.matmul(frame,self.W_q)
-            Key = torch.matmul(frame,self.W_k)
-            Value = torch.matmul(frame,self.W_v)
+        for j in range(frame.size(0)):
+            batch_outputs = []
+            for i in range(self.heads):
+                Query = torch.mm(frame[j],self.W_q)
+                Key = torch.mm(frame[j],self.W_k)
+                Value = torch.mm(frame[j],self.W_v)
 
-            single_head_output = self.self_attention(Query,Key,Value)
-            outputs.append(single_head_output)
-        heads_concat_output = torch.cat(outputs,axis=1)
-
-        multi_head_output = torch.matmul(heads_concat_output,self.W_o.cuda())
-        multi_head_output = multi_head_output + frame.cuda()
-        layer_normalization1 = torch.nn.LayerNorm(512).cuda()
-        multi_head_output = layer_normalization1(multi_head_output.type(torch.cuda.FloatTensor)) #the error says "RuntimeError: expected scalar type Double but found Float" but actually works if I cast to float from double, bello
-            
+                single_head_output = self.self_attention(Query,Key,Value)
+                batch_outputs.append(single_head_output)
+            heads_concat_output = torch.cat(batch_outputs,axis=1)
+            #print("heads out", heads_concat_output.size())
+            outputs.append(heads_concat_output)
+        outputs = torch.stack(outputs,dim=0)
+        print("ouputs size",outputs.size())
+        multi_head_output = torch.mm(heads_concat_output,self.W_o)
+        multi_head_output = multi_head_output + frame
+        multi_head_output = self.layer_normalization1(multi_head_output) #the error says "RuntimeError: expected scalar type Double but found Float" but actually works if I cast to float from double, bello    
         
         multi_head_output = torch.unsqueeze(multi_head_output,2)
         return multi_head_output
@@ -62,17 +68,18 @@ class MyTransformer(nn.Module):
     def self_attention(self,Query,Key,Value):
         Key_T = torch.transpose(Key,0,1)
         Query_Key_T = torch.div(torch.matmul(Query,Key_T),math.sqrt(self.d_k))
-        attention = F.softmax(Query_Key_T.type(torch.cuda.DoubleTensor),dim=-1)
-        self_attention_output = torch.matmul(attention,Value.cuda())
+        attention = F.softmax(Query_Key_T,dim=-1)
+        self_attention_output = torch.matmul(attention,Value)
         return self_attention_output
 
 
     def MLP_head(self,multi_head_output):
-        out = self.classifier(multi_head_output.type(torch.cuda.FloatTensor))
+        print("multihead", multi_head_output.size())
+        out = self.classifier(multi_head_output.squeeze(2).view(32,-1))
+        print("out size: ",out.size())
         out = out + multi_head_output
-        
-        layer_normalization2 = torch.nn.LayerNorm(512).cuda()
-        out = layer_normalization2(out.squeeze(2).type(torch.cuda.FloatTensor))
+    
+        out = self.layer_normalization2(out.squeeze(2))
         out = torch.mean(out,0).view(-1) #not really sure if mean is the best thing, BERT and VTN use the CLS token
         return out
     
@@ -93,4 +100,3 @@ class MyTransformer(nn.Module):
         pos_encoding = angle_rads[np.newaxis, ...]
         pos_encoding = torch.tensor(pos_encoding).cuda()
         return pos_encoding
-
