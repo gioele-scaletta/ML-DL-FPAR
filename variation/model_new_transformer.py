9378e8771500fdbf6543337a945632dee716e15b
genpet761 <84572515+genpet761@users.noreply.github.com>
2021-06-30 10:47:23 +0200

    Update model_new_transformer.py

--- variation/model_new_transformer.py
+++ variation/model_new_transformer.py
@@ -30,11 +30,9 @@ class selfAttentionModel(nn.Module):
         feats_ss = []
         embeddings = []
         bz, nf, nc_rgb, h, w = inputVariable.size()
-        print("input variable: ",inputVariable.size())
         inputVariable = inputVariable.view(-1,nc_rgb,h,w)
         logit, feature_conv, feature_convNBN = self.resNet(inputVariable)
         bz_nf, nc_resnet, h, w = feature_conv.size()    #bz = batch size    #nc = number of channels    #h = height   #w = width
-        print("feature conv", feature_conv.size())
         feature_conv1 = feature_conv.view(bz_nf, nc_resnet, h*w)    #make vector from matrix (hxw -> h*w)
         probs, idxs = logit.sort(1, True)   #sort in order from highest to lowest the score of each class in each batch
         class_idx = idxs[:, 0]    
@@ -42,16 +40,10 @@ class selfAttentionModel(nn.Module):
         attentionMAP = F.softmax(cam.squeeze(1), dim=1)
         attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
         attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
-        print("attention feat:",attentionFeat.size())
         embeddings = torch.squeeze(torch.squeeze(self.avgpool(attentionFeat),3),2)
         embeddings = embeddings.view(bz,nf,-1)
         logit = self.transf(embeddings)
-        print("logit",logit.size())
         final_logit = self.fc(logit.cuda())
-        print("final logit",final_logit.size())
         ss_task_feats = self.ss_task(feature_conv) # a tensor of size [32,7*7] is returned
-        print("ss task feats",ss_task_feats.size())
         feats_ss = ss_task_feats.view(bz, nf, 7*7) #now that it is a regression problem no more 2 and no more softmax needed
-        print("feats self super", feats_ss.size())
-        print("_"*50)
         return final_logit, feats_ss
